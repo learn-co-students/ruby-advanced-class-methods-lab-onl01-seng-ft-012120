45f64a3a91dfb50db1350f196eff36a5ba547ad5
Diego Vasquez <botellongo69@outlook.com>
2020-01-31 22:59:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,40 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    
+  end
+  
+  def self.new_by_name(song_name)
+    
+  end
+  
+  def self.create_by_name(song_name)
+    
+  end
+  
+  def self.find_by_name(song_name)
+    
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    
+  end
+  
+  def self.alphabetical
+    
+  end
+  
+  def self.new_from_filename(filename)
+    
+  end
+  
+  def self.create_from_filename(filename)
+    
+  end
+  
+  def  self.destroy_all
+    @@all.clear
+  end
 end
