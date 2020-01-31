305b2d9ed84785ee4e9357ad2f330d7dff62b446
Kyle McKenna <mckennakyler@gmail.com>
2020-01-31 19:24:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -44,20 +44,20 @@ class Song
     self.all.sort_by {|song| song.name}
   end 
   
-  def self.new_from_filename(filename)
+  def self.new_from_filename(filename)   #.new_from_filename(filename)
+    song = self.new  #song = self.new
     song_array = filename.split(" - ")
-    song_array[1] = song_array[1].chomp(".mp3")
-    song = self.new
-    song.name = song_array[1]
-    song.artist_name = song_array[0] 
+    song_array[1] = song_array[1].chomp(".mp3")  #song = song.new_from_filename("Thundercat - For Love I Come.mp3")
+    song.name = song_array[1]  #song.name = "For Love I Come"
+    song.artist_name = song_array[0]   #song.artist_name = "Thundercat"
     song
   end  
-
-  def self.create_from_filename(filename)
-    result = self.new_from_filename(name)
-    song = self.create 
-    song.name = result.name 
-    song.artist_name = result.artist_name
+  
+  def self.create_from_filename(filename) #.create_from_filename(filename)
+    files = self.new_from_filename(filename) #song_match = song.find_by_name("For Love I Come")
+    song = self.create #song = song.create_from_filename("Thundercat - For Love I Come.mp3")
+    song.name = files.name #   song_match.name = ("For Love I Come")
+    song.artist_name = files.artist_name   # song_match.artist_name = ("Thundercat")
     song
   end 
 
