b6a8a6d8ece9d5c43e64cb7784b7a7a4dbc92b51
Diego Vasquez <botellongo69@outlook.com>
2020-02-01 01:42:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -11,11 +11,15 @@ class Song
   end
   
   def self.create
-    
+    song = self.new
+    @@all << song
+    song
   end
   
   def self.new_by_name(song_name)
-    
+    song = self.new
+    song.name = song_name
+    song
   end
   
   def self.create_by_name(song_name)
